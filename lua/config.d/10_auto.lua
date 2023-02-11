e6980d434820294d31b88f504d0589f1899d7858
J Christianson <j_christiansen@apple.com>
2023-02-10 19:27:28 -0600

    add autoFormat augroup & netrw to `q` for quit

--- lua/config.d/10_auto.lua
+++ lua/config.d/10_auto.lua
@@ -4,16 +4,18 @@ vim.api.nvim_create_autocmd("FileType", {
         "help",
         "fugitive",
         "gitcommit",
+        "netrw",
     },
-    -- command = "nnoremap <buffer> q :close<cr>"
     callback = function()
         vim.keymap.set("n", "q", ":close<cr>", { buffer = true, silent = true })
     end
 })
 
 -- Run lsp formatter on write
+local autoFormatGroup = vim.api.nvim_create_augroup("autoFormat", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePre",
     {
+        group = autoFormatGroup,
         callback = function()
             if not string.match("scratchpad", vim.api.nvim_buf_get_name(0)) then
                 vim.lsp.buf.format()
