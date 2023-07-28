5ae9b3e02f0da2ccbb728975dc1c2070831415bf
J Christianson <j_christiansen@apple.com>
2023-07-28 15:10:03 -0500

    nvim-dap-go & slight reorganization

--- lua/plugins.d/04_plugins_dap.lua
+++ lua/plugins.d/04_plugins_dap.lua
@@ -1,5 +1,11 @@
 plugins.dap = {
     {
+        'rcarriga/nvim-dap-ui'
+    },
+    {
+        'theHamsta/nvim-dap-virtual-text'
+    },
+    {
         "mfussenegger/nvim-dap",
         config = function()
             local dap = require("dap")
@@ -9,23 +15,6 @@ plugins.dap = {
             vim.fn.sign_define("DapBreakpointRejected", { text = "", texthl = "red" })
             vim.fn.sign_define("DapLogPoint", { text = "", texthl = "yellow" })
             vim.fn.sign_define("DapStopped", { text = "", texthl = "green" })
-
-            dap.adapters.go = function(callback, _)
-                local handle
-                local port = 38697
-                handle, _ = vim.loop.spawn("dlv", {
-                    args = { "dap", "-l", "127.0.0.1:" .. port },
-                    detached = true,
-                }, function(code)
-                    handle:close()
-                    print("Delve exited with exit code: " .. code)
-                end)
-                -- Wait 100ms for delve to start
-                vim.defer_fn(function()
-                    dap.repl.open()
-                    callback({ type = "server", host = "127.0.0.1", port = port })
-                end, 100)
-            end
 
             -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
             require("dapui").setup({
@@ -75,8 +64,8 @@ plugins.dap = {
                     },
                 },
                 floating = {
-                    max_height = 1, -- These can be integers or a float between 0 and 1.
-                    max_width = 19, -- Floats will be treated as percentage of your screen.
+                    max_height = 1,    -- These can be integers or a float between 0 and 1.
+                    max_width = 19,    -- Floats will be treated as percentage of your screen.
                     border = "single", -- Border style. Can be "single", "double" or "rounded"
                     mappings = {
                         close = { "q", "<Esc>" },
@@ -85,78 +74,50 @@ plugins.dap = {
                 windows = { indent = 1 },
             })
 
-            dap.adapters.dlv_spawn = function(cb)
-                local stdout = vim.loop.new_pipe(false)
-                local handle
-                local pid_or_err
-                local port = 38697
-                local opts = {
-                    stdio = { nil, stdout },
-                    args = { "dap", "-l", "127.0.0.1:" .. port },
-                    detached = true,
-                }
-                handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
-                    stdout:close()
-                    handle:close()
-                    if code ~= 0 then
-                        print("dlv exited with code", code)
-                    end
-                end)
-                assert(handle, "Error running dlv: " .. tostring(pid_or_err))
-                stdout:read_start(function(err, chunk)
-                    assert(not err, err)
-                    if chunk then
-                        vim.schedule(function()
-                            --- You could adapt this and send `chunk` to somewhere else
-                            require("dap.repl").append(chunk)
-                        end)
-                    end
-                end)
-                -- Wait for delve to start
-                vim.defer_fn(function()
-                    cb({ type = "server", host = "127.0.0.1", port = port })
-                end, 100)
-            end
-
-            dap.configurations.go = {
-                {
-                    type = "dlv_spawn",
-                    name = "Launch dlv & file",
-                    request = "launch",
-                    program = "${workspaceFolder}",
-                },
-                {
-                    type = "go",
-                    name = "Debug",
-                    request = "launch",
-                    program = "${workspaceFolder}",
-                },
-                {
-                    type = "dlv_spawn",
-                    name = "Debug with arguments",
-                    request = "launch",
-                    program = "${workspaceFolder}",
-                    args = function()
-                        local args_string = vim.fn.input("Arguments: ")
-                        return vim.split(args_string, " +")
-                    end,
-                },
-                {
-                    type = "go",
-                    name = "Debug test",
-                    request = "launch",
-                    mode = "test", -- Mode is important
-                    program = "${file}",
-                },
-            }
-
             require("nvim-dap-virtual-text").setup({})
         end
     },
     {
-        'rcarriga/nvim-dap-ui'
-    },
-    {
-        'theHamsta/nvim-dap-virtual-text'
+        "leoluz/nvim-dap-go",
+        ft = { "go" },
+        config = function()
+            require("dap-go").setup({
+                -- Additional dap configurations can be added.
+                -- dap_configurations accepts a list of tables where each entry
+                -- represents a dap configuration. For more details do:
+                -- :help dap-configuration
+                dap_configurations = {
+                    {
+                        -- Must be "go" or it will be ignored by the plugin
+                        type = "go",
+                        name = "Attach remote",
+                        mode = "remote",
+                        request = "attach",
+                    },
+                },
+                -- delve configurations
+                delve = {
+                    -- the path to the executable dlv which will be used for debugging.
+                    -- by default, this is the "dlv" executable on your PATH.
+                    path = "dlv",
+                    -- time to wait for delve to initialize the debug session.
+                    -- default to 20 seconds
+                    initialize_timeout_sec = 20,
+                    -- a string that defines the port to start delve debugger.
+                    -- default to string "${port}" which instructs nvim-dap
+                    -- to start the process in a random available port
+                    port = "${port}",
+                    -- additional args to pass to dlv
+                    args = {},
+                    -- the build flags that are passed to delve.
+                    -- defaults to empty string, but can be used to provide flags
+                    -- such as "-tags=unit" to make sure the test suite is
+                    -- compiled during debugging, for example.
+                    -- passing build flags using args is ineffective, as those are
+                    -- ignored by delve in dap mode.
+                    build_flags = "",
+                },
+            })
+        end
     },
 }
