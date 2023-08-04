0755b25c8814b999a6cd1d0aaf3bda9f1a843c76
J Christianson <j_christiansen@apple.com>
2023-08-04 11:00:21 -0500

    set autocmd vars to local

--- lua/config.d/10_auto.lua
+++ lua/config.d/10_auto.lua
@@ -18,7 +18,7 @@ vim.api.nvim_create_autocmd("BufWritePre",
     {
         group = autoFormatGroup,
         callback = function()
-            filetype = vim.bo[0].filetype
+            local filetype = vim.bo[0].filetype
             if not string.match("scratchpad", vim.api.nvim_buf_get_name(0)) then
                 if filetype == "go" then
                     require('go.format').goimport()
@@ -35,7 +35,7 @@ vim.api.nvim_create_autocmd("BufWritePost",
         group = postWrite,
         callback = function()
             local job = require("plenary.job")
-            filetype = vim.bo[0].filetype
+            local filetype = vim.bo[0].filetype
             if not string.match("scratchpad", vim.api.nvim_buf_get_name(0)) then
                 if filetype == "go" then
                     job:new({
