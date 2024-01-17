883ef2748a1c49f3d89c083650f1e2054402a373
P-O-T-A-T-O <voesec@gmail.com>
2024-01-16 22:03:44 -0600

    bug: broke highlighting with ts

--- lua/plugins.d/02_plugins_treesitter.lua
+++ lua/plugins.d/02_plugins_treesitter.lua
@@ -66,7 +66,7 @@ plugins.treesitter = {
                 sync_install = false,
                 highlight = {
                     enable = true,
-                    additional_vim_regex_highlighting = true,
+                    --     additional_vim_regex_highlighting = true,
                 },
                 rainbow = {
                     enable = true,
